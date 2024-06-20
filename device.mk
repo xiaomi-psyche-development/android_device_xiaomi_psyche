de67a7ef4907eb9a53007bf48d134e27a6907b98
RITEFANG <1721985272@qq.com>
2024-06-20 09:01:49 +0800

    psyche: Set A/B for true

--- device.mk
+++ device.mk
@@ -7,10 +7,13 @@
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
+# A/B
+TARGET_IS_VAB := true
+
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
-    
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
