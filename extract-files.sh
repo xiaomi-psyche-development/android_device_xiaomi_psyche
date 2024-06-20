3fea01079dfd9130c3454de0fa0c8467b15c5f3b
RITEFANG <1721985272@qq.com>
2024-06-20 15:34:26 +0800

    psyche: Drop selinux context from battery init unit

--- extract-files.sh
+++ extract-files.sh
@@ -7,6 +7,10 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/etc/init/init.batterysecret.rc)
+            sed -i "/seclabel u:r:batterysecret:s0/d" "${2}"
+            ;;
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
