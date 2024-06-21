f52bcd02ebeba1e426efc9a62e8c7802646529b6
RITEFANG <1721985272@qq.com>
2024-06-21 17:57:50 +0800

    psyche: Add psyche to assert

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,9 @@ TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_psyche
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/xiaomi/psyche.config
 
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := psyche
+
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
